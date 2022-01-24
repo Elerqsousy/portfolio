4c6ab54917ded2f19774acb049682b9613349e69
Bavon101 <bavonfate@gmail.com>
2022-01-24 22:11:16 +0300

    Fix: Lint errors

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-let menu1 = document.querySelector('.nav-btn');
-let menu2 = document.querySelector('.close-drawer-btn');
-let navBars = document.querySelector(".open-nav");
+const menu1 = document.querySelector('.nav-btn');
+const menu2 = document.querySelector('.close-drawer-btn');
+const navBars = document.querySelector('.open-nav');
 
 function disappear() {
-    let menuDis1 = document.querySelector('.navbar');
+    const menuDis1 = document.querySelector('.navbar');
     menuDis1.classList.toggle('disappear');
-    let menuDis2 = document.querySelector('.open-nav');
+    const menuDis2 = document.querySelector('.open-nav');
     menuDis2.classList.toggle('disappear');
     menuDis2.classList.toggle('menu-drawer-height');
-};
-
-menu1.addEventListener('click', disappear);
-menu2.addEventListener('click', disappear);
-navBars.addEventListener('click', closeOnAtag, false);
+}
 
 function closeOnAtag(e) {
-    var anchor = e.target.closest('a');
+    const anchor = e.target.closest('a');
     if (anchor !== null) {
         disappear();
     }
 }
+menu1.addEventListener('click', disappear);
+menu2.addEventListener('click', disappear);
+navBars.addEventListener('click', closeOnAtag, false);
