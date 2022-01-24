1700d12ffc3d3e3304fc13e18b1a22b11ddb32ec
Bavon101 <bavonfate@gmail.com>
2022-01-24 18:56:13 +0300

    Add: Eventlistener to close drawer on menu item click

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let menu1 = document.querySelector('.nav-btn');
 let menu2 = document.querySelector('.close-drawer-btn');
+let navBars = document.querySelector(".open-nav");
+
 function disappear() {
     let menuDis1 = document.querySelector('.navbar');
     menuDis1.classList.toggle('disappear');
     let menuDis2 = document.querySelector('.open-nav');
     menuDis2.classList.toggle('disappear');
     menuDis2.classList.toggle('menu-drawer-height');
-    console.log("I am working");
-    };
+};
 
-menu1.addEventListener('click',disappear);
-menu2.addEventListener('click',disappear);
+menu1.addEventListener('click', disappear);
+menu2.addEventListener('click', disappear);
+navBars.addEventListener('click', closeOnAtag, false);
 
+function closeOnAtag(e) {
+    var anchor = e.target.closest('a');
+    if (anchor !== null) {
+        disappear();
+    }
+}
